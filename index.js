6ec391ad6e4cf00aee6500c4b824516ad6fa06fa
DavidMendiluce <davidmendiluce@gmail.com>
2022-01-05 20:42:36 +0100

    changes in index.js file for deployment

--- index.js
+++ index.js
@@ -9,10 +9,16 @@ var locationsController = require('./controllers/locationsController.js');
 const userRoutes = require('./routes/user');
 
 var app = express();
+
+app.use(express.static('./dist/'))
 app.use(bodyParser.json());
 app.use(cors({origin: 'http://localhost:4200'}));
 
-app.listen(3000, () => console.log('Server started at port: 3000'));
+/*app.listen(3000, () => console.log('Server started at port: 3000'));*/
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 app.use('/appointments', appointmentsController);
 
