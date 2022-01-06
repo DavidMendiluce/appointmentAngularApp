78920d613a0988226eac571b310c2d63ae4fc704
DavidMendiluce <davidmendiluce@gmail.com>
2022-01-06 19:05:10 +0100

    modified index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(cors({origin: 'http://localhost:4200'}));
 
-app.listen(3000, () => console.log('Server started at port: 3000'));
+//app.listen(3000, () => console.log('Server started at port: 3000'));
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
